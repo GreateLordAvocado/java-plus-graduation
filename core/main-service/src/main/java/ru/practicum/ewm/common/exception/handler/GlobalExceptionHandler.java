4d60c673ac7f91e4e7921ad3ce8b44c1fc5a9fba
GreateLordAvocado <sinner.halo@yandex.ru>
2026-03-29 12:01:10 +0700

    fix: добавлено логирование исключений в GlobalExceptionHandler

--- core/main-service/src/main/java/ru/practicum/ewm/common/exception/handler/GlobalExceptionHandler.java
+++ core/main-service/src/main/java/ru/practicum/ewm/common/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.practicum.ewm.common.exception.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,25 +18,30 @@ import static ru.practicum.stats.common.Constants.DATE_TIME_FORMAT;
 
 @ControllerAdvice
 public final class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("Validation error", e);
         return buildBadRequest(e);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiError> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.warn("Method argument type mismatch", e);
         return buildBadRequest(e);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiError> handleBadRequestException(BadRequestException e) {
+        log.warn("Bad request", e);
         return buildBadRequest(e);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> handleNotFoundException(NotFoundException e) {
+        log.warn("Object not found", e);
         final ApiError error = ApiError.builder()
                 .status("NOT_FOUND")
                 .reason("The required object was not found")
@@ -46,6 +53,7 @@ public final class GlobalExceptionHandler {
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ApiError> handleConflictException(ConflictException e) {
+        log.warn("Conflict while processing request", e);
         final ApiError error = ApiError.builder()
                 .status("CONFLICT")
                 .reason("For the requested operation the conditions are not met")
