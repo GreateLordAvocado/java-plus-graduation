67140ece16ae2304cbaee0944f8cb4ef93825252
GreateLordAvocado <sinner.halo@yandex.ru>
2026-03-29 12:24:43 +0700

    fix: добавлена fail-fast проверка null в UrlUtils

--- core/main-service/src/main/java/ru/practicum/ewm/event/util/UrlUtils.java
+++ core/main-service/src/main/java/ru/practicum/ewm/event/util/UrlUtils.java
@@ -3,12 +3,12 @@ package ru.practicum.ewm.event.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UrlUtils {
     public static String removeTrailingNumberSegment(String url) {
-        if (url == null) {
-            return null;
-        }
+        Objects.requireNonNull(url, "url must not be null");
         return url.replaceAll("/\\d+$", "");
     }
 }
