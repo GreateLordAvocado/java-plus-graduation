7682eac7b4768fbb6426e9226ac273edd04e68e7
GreateLordAvocado <sinner.halo@yandex.ru>
2026-04-04 03:34:22 +0700

    fix: отключен самостоятельный spring boot запуск catalog-service при встраивании в event-service

--- core/catalog-service/src/main/java/ru/practicum/ewm/catalog/CatalogServiceApplication.java
+++ core/catalog-service/src/main/java/ru/practicum/ewm/catalog/CatalogServiceApplication.java
@@ -1,21 +1,6 @@
 package ru.practicum.ewm.catalog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.practicum.ewm.catalog.category.service.EventCategoryUsageClient;
-import ru.practicum.ewm.catalog.compilation.service.CompilationEventInternalClient;
-
-@SpringBootApplication(scanBasePackages = {
-        "ru.practicum.ewm.catalog",
-        "ru.practicum.ewm.common"
-})
-@EnableFeignClients(basePackageClasses = {
-        EventCategoryUsageClient.class,
-        CompilationEventInternalClient.class
-})
 public class CatalogServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
