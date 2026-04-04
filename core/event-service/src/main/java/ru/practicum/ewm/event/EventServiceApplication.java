a67493a2081a9d8b368ccdbc1bcd7d04f7938fed
GreateLordAvocado <sinner.halo@yandex.ru>
2026-04-05 00:24:36 +0700

    fix: восстановлено создание stats-client бина в event-service

--- core/event-service/src/main/java/ru/practicum/ewm/event/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/ewm/event/EventServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"ru.practicum.ewm", "ru.practicum.stats"})
+@EnableFeignClients(basePackages = {"ru.practicum.ewm", "ru.practicum.stats"})
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
